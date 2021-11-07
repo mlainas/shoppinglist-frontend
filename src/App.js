b0f37e4b9f1f01f1579f49c934a98568c27e9f2c
Mikko Lainas <n0lami00@students.oamk.fi>
2021-11-07 23:18:20 +0200

    valmis...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const URL = 'http://localhost/shoppinglist/';
@@ -10,24 +10,24 @@ function App() {
   const [items, setItems] = useState([]);
   const [item, setItem] = useState('');
   const [items2, setItems2] = useState([]);
-  const [item2,setItem2] = useState('');
+  const [item2, setItem2] = useState('');
 
   useEffect(() => {
     axios.get(URL)
-    .then((response) => {
-      setItems(response.data);
-    }).catch(error => {
-      alert(error.response ? error.response.data.error : error);
-    })
-  },[]);
+      .then((response) => {
+        setItems(response.data);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      })
+  }, []);
   useEffect(() => {
     axios.get(URL)
-    .then((response) => {
-      setItems2(response.data);
-    }).catch(error => {
-      alert(error.response ? error.response.data.error : error);
-    })
-  },[]);
+      .then((response) => {
+        setItems2(response.data);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      })
+  }, []);
 
 
   function remove(id) {
@@ -48,87 +48,47 @@ function App() {
 
   function save(e) {
     e.preventDefault();
-    const json = JSON.stringify({description:item})
-    axios.post(URL + 'add.php',json,{
+    const json = JSON.stringify({ description: item })
+    axios.post(URL + 'add.php', json, {
       headers: {
-        'Content-Type' : 'application/json'
+        'Content-Type': 'application/json'
       }
     })
-    .then((response) => {
-      console.log(response);
-      setItems(items => [...items,response.data]);
-      setItem('');
-    }).catch (error => {
-      alert(error.response.data.error)
-    });
+      .then((response) => {
+        console.log(response);
+        setItems(items => [...items, response.data]);
+        setItem('');
+      }).catch(error => {
+        alert(error.response.data.error)
+      });
 
   }
-  /* function save2(e) {
-    e.preventDefault();
-    const json = JSON.stringify({description:item2})
-    axios.post(URL + 'add.php',json,{
-      headers: {
-        'Content-Type' : 'application/json'
-      }
-    })
-    .then((response) => {
-      console.log(response);
-      setItems2(items2 => [...items2,response.data]);
-      setItem2('');
-    }).catch (error => {
-      alert(error.response.data.error)
-    });
-  } */
+
   return (
     <div className="container">
-    <h3> Shopping list </h3>
-    <form onSubmit={save}>
-      <label> New item </label>
-      <input value={item} onChange={e => setItem(e.target.value)} placeholder="Type description"/>
-      <input value={item2} onChange={e => setItem2(e.target.value)} placeholder="Type amount"/>
-      <button> Add </button>
-    </form>
-    <ul>
-      {items?.map(item => (
-        <li key={item.id}>{item.description}</li>
-      ))}
-    </ul>
-    <ul>
-      {items2?.map(item => (
-        <li key={item.id}>{item.item}
-          <a href="#" className="delete" onClick={() => remove(item.id)}>Delete</a>
-        </li>
-      ))}
-    </ul>
-  </div>
-  );
-      }
-    {/* <div className="container">
-      <h3>Shoppinglist</h3>
+      <h3> Shopping list </h3>
       <form onSubmit={save}>
-        <label>New item</label>
-        <input value={item} onChange={e => setItem(e.target.value)} placeholder="type description" />
-        <input value={item2} onChange={e => setItem2(e.target.value)} placeholder="type amount" />
-        <button>Add</button>
+        <label> New item </label>
+        <input value={item} onChange={e => setItem(e.target.value)} placeholder="Type description" />
+        <input value={item2} onChange={e => setItem2(e.target.value)} placeholder="Type amount" />
+        <button> Add </button>
       </form>
-      <ol>
+      <ul>
         {items?.map(item => (
-          <div key={item.id}>
-            {item.description}&nbsp;
-            <a href="#" className="delete" onClick={() => remove(item.id)}>
-              Delete
-              </a>
-            </div>
+          <li key={item.id}>{item.description}</li>
         ))}
-        </ol>
-        
-        <ol>
-         {items2?.map(item2 => (
-          <div key={item2.id}>{item2.amount}</div>
+      </ul>
+      <ul>
+        {items2?.map(item => (
+          <li key={item.id}>{item.item}
+            <a href="#" className="delete" onClick={() => remove(item.id)}>Delete</a>
+          </li>
         ))}
-      </ol>
+      </ul>
     </div>
   );
-} */}
+}
+
+
 
 export default App;
